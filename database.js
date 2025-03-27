e366c5e6d667934f958323528cc94451db2089d4
HenrikBaltazar <henrikgomesbaltazar@gmail.com>
2025-03-27 19:49:54 -0300

    add orm

--- database.js
+++ database.js
@@ -10,13 +10,24 @@ const sequelize = new Sequelize(DATABASE, MYSQL_LOGIN, MYSQL_PASSWORD, {
     dialect: "mysql"
 });
 
-const Customer = sequelize.define('Customer', {
-    customer_id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
-    first_name: { type: DataTypes.STRING, allowNull: false },
-    last_name: { type: DataTypes.STRING, allowNull: false },
-    email: { type: DataTypes.STRING },
-    address_id: { type: DataTypes.INTEGER }
-}, { tableName: 'customer', timestamps: false });
+const Store = sequelize.define('Store', {
+    store_id: { type: DataTypes.TINYINT.UNSIGNED, autoIncrement: true, primaryKey: true },
+    manager_staff_id: { type: DataTypes.TINYINT.UNSIGNED, allowNull: false, unique: true },
+    address_id: { type: DataTypes.SMALLINT.UNSIGNED, allowNull: false }
+}, { tableName: 'store', timestamps: false });
+
+const Manager = sequelize.define('Manager', {
+    staff_id: { type: DataTypes.TINYINT.UNSIGNED, autoIncrement: true, primaryKey: true },
+    address_id: { type: DataTypes.SMALLINT.UNSIGNED, allowNull: false  },
+    first_name: { type: DataTypes.STRING, allowNull: false  },
+    last_name: { type: DataTypes.STRING, allowNull: false  },
+    picture: { type: DataTypes.BLOB, allowNull: true, defaultValue: null  },
+    store_id: { type: DataTypes.TINYINT.UNSIGNED, allowNull: false },
+    active: { type: DataTypes.BOOLEAN, allowNull: false, defaultValue:true  },
+    email: { type: DataTypes.STRING, allowNull: true, defaultValue: null  },
+    username: { type: DataTypes.STRING, allowNull: false  },
+    password: { type: DataTypes.STRING, allowNull: true, defaultValue: null },
+}, { tableName: 'staff', timestamps: false });
 
 const Address = sequelize.define('Address', {
     address_id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
@@ -25,6 +36,10 @@ const Address = sequelize.define('Address', {
     district: { type: DataTypes.STRING }
 }, { tableName: 'address', timestamps: false });
 
-Customer.hasOne(Address, { foreignKey: 'address_id' });
+Store.hasOne(Address, { foreignKey: 'address_id' });
+Store.hasOne(Manager, { foreignKey: 'manager_staff_id' });
 
-module.exports = { sequelize, Customer, Address };
+Manager.hasOne(Store, { foreignKey: 'store_id' });
+Manager.hasOne(Address, { foreignKey: 'address_id' });
+
+module.exports = { sequelize, Store, Manager, Address };
